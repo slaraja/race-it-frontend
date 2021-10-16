374706eb361d2de0b84a629ea1d6993a5d5919ee
Stephanie La Raja <stephanielaraja@gmail.com>
2021-10-16 16:28:31 -0400

    start race card function

--- src/RacesContainer.js
+++ src/RacesContainer.js
@@ -1,10 +1,18 @@
 import React from "react";
 
+//need to use class if utilize state
 class RacesContainer extends React.Component{
 
+    //initial state
     state = {
         races: []
+        //when we get back our fetch, we can update this
     }
+
+    makeRaceCard(){
+        this.state.map(race => )
+    }
+
 
     render(){
         return(
