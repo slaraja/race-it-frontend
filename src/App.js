76e82d9d02e1fc5702a511d568b464d0faa64348
Stephanie La Raja <stephanielaraja@gmail.com>
2021-10-16 11:33:38 -0400

    create racescontainer and import/export

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
 import Race from "./Race";
+import RacesContainer from "./RacesContainer";
+
 
 //this functional component is returning HTML
 function App() {
@@ -8,10 +10,10 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>
-       Testing my app 
-        </h1>
+        <h1> Race It </h1>
+        <h2> Find your next running event! </h2>
         < Race />
+        < RacesContainer />
         <a
           className="App-link"
           href="https://reactjs.org"
