c9bb506aca21e84895c301f52fe3d99f8545151e
Stephanie La Raja <stephanielaraja@gmail.com>
2021-11-01 07:14:43 -0400

    display location attributes

--- src/components/RaceCard.js
+++ src/components/RaceCard.js
@@ -2,7 +2,7 @@ function RaceCard(props){
     
     return(
         <div className="card" id={`race-${props.id}`}>
-            <h1>{props.name}</h1>
+            <h1>{props.name} - {props.city}, {props.state} {props.zipcode}</h1>
         </div>
     )
 }
