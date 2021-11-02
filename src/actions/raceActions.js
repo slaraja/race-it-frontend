e5f9bd7ee106573871decb751f9aed5712ec6a0c
Stephanie La Raja <stephanielaraja@gmail.com>
2021-11-02 12:59:04 -0400

    clean up comments in raceActions

--- src/actions/raceActions.js
+++ src/actions/raceActions.js
@@ -1,26 +1,18 @@
-//action creator - a function that returns an object
-//type property that tells the action what to do
 export const startRaceFetch = (races) => ({type: "START_FETCHING_RACES", payload: races})
 
 export const addRace = (race) => ({type: "ADDED_RACE", payload: race})
 
-//thunk gives ability to return a function with a default arg of dispatch
 export const fetchRaces = () => {
     return (dispatch) => {
         fetch("http://localhost:3000/races")
         .then(resp => resp.json())
-        //converts stringified format to json
         .then(json => {
-            //happens async
-            //causes a re-render
-            dispatch(startRaceFetch(json)) //calling reducer function
+            dispatch(startRaceFetch(json)) 
         })
-        //handle the json, update state
     }
 }
 
-export const createRaceFetch = (race) => {    //return a function that is going to get called with receiving dispatch as an arg
-    console.log("#2")
+export const createRaceFetch = (race) => {   
     return (dispatch) => {
         const configObj = {
             method: 'POST',
@@ -33,11 +25,8 @@ export const createRaceFetch = (race) => {    //return a function that is going 
         fetch("http://localhost:3000/races", configObj)
         .then(resp => resp.json())
         .then(json => {
-            dispatch(addRace(json)) //calling reducer funciton
-            console.log("#6")  //.then takes time to process
-
+            dispatch(addRace(json))
         })
-        console.log("#3")
 
     }
 
